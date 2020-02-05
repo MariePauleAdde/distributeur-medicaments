aa772788afdafdc5560448f8e6977ea907ce72d1
MariePauleAdde <58311031+MariePauleAdde@users.noreply.github.com>
2020-02-05 16:00:36 +0100

    Add files via upload

---
+++ Code relatif/1erTestFinal.ino
@@ -0,0 +1,138 @@
+//LIBRAIRIES:
+
+//ecran LCD
+#include <LiquidCrystal.h>
+
+//horloge/temps
+#include <Wire.h>
+#include "RTClib.h"
+#include <TimeLib.h>
+#include<DS1307RTC.h>
+RTC_DS1307 rtc;
+
+//buzzer
+#include "pitches.h"
+
+//PARAMETRES moteur pas à pas 
+#define IN1 8
+#define IN2 9
+#define IN3 10
+#define IN4 12
+int temps =10;
+int i=0;
+
+//PARAMETRES ECRAN LCD
+  LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // initialise les commandes avec les n° de broches
+  
+// notes in the melody:
+int melody[] = {
+  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
+int duration = 1000;  // 500 miliseconds
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  //SET UP LCD
+  lcd.begin(16, 2); 
+  lcd.print("Il est :"); 
+  
+  //SET UP MOTEUR 
+pinMode(IN1, OUTPUT);
+pinMode(IN2, OUTPUT);
+pinMode(IN3, OUTPUT);
+pinMode(IN4, OUTPUT);
+
+}
+
+//Fonction pour corriger l'affichage de l'heure sur l'écran lcd 
+void print3digits(int number) {
+  if (number >= 0 && number < 10) {
+    lcd.print('0');
+  }
+  lcd.print(number);
+}
+
+//Fonction qui fiche l'heure sur l'écran LCD
+void printOnLCD(){
+  tmElements_t tm;
+  if (RTC.read(tm)) {
+  lcd.setCursor(0, 1); //le curseur se positionne à la 1ère colonne, 2ième ligne 
+  print3digits(tm.Hour);
+  lcd.print("h");
+  print3digits(tm.Minute);
+  }
+}
+
+//Fonction qui à l'heure de prise du médicament va mettre en rotation le moteur et lancer une alarme 
+void heureDePrise(int heureMatin, int minuteMatin, int heureMidi, int minuteMidi ,int heureSoir, int minuteSoir){
+  DateTime now = rtc.now();
+  tmElements_t tm;
+  Serial.println(now.hour());
+  Serial.println(now.minute());
+
+  if ((now.hour() == heureMatin && now.minute() == minuteMatin) or (now.hour()==heureMidi && now.minute()==minuteMidi) or (now.hour()==heureSoir && now.minute()==minuteSoir)) {
+
+    //Test sur moniteur série
+    Serial.println("C'est bon");
+
+    //Mettre le buzzer en marche 
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+    // pin8 output the voice, every scale is 0.5 sencond
+    tone(11, melody[thisNote], duration);
+     
+    // Output the voice after several minutes
+    delay(5);
+    }
+    
+    //Afficher sur l'écran lcd 
+    lcd.clear();
+    lcd.print("Prenez votre");
+    lcd.setCursor(0, 1);
+    lcd.print("medicament");
+    
+    //Mettre le moteur en marche 
+    for (i=0; i <= 511; i++){
+      Serial.println(i);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      delay(temps);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      delay(temps); 
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      delay(temps);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      delay(temps); 
+    }
+
+  }
+  else{
+    Serial.println("L'alarme ne doit pas sonner");
+    lcd.clear();
+    lcd.print("Il est:");
+    
+  }
+}
+
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printOnLCD();
+  heureDePrise(15,17, 12,16,20,35);
+}
